9e151cb49f03158752e9a614e7d336209f0add6f
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-06-07 09:56:52 +0900

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -2,31 +2,35 @@ defmodule Gimei.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :gimei,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :gimei,
+      version: "0.0.1",
+      elixir: "~> 1.0.0",
+      description: "Gimei is a pure Elixir library for genrating Japanese fake data.",
+      package: package,
+      name: "Gimei",
+      source_url: "https://github.com/KazuCocoa/elixir-gimei",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "mix.lock"],
+      contributors: ["Kazuaki Matsuo"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/KazuCocoa/elixir-gimei"}
+    ]
   end
 end
