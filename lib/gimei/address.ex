fb01eb24f059423ba531d1576a8c2f73b4d95ca6
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2015-06-09 07:41:10 +0900

    add todo

--- lib/gimei/address.ex
+++ lib/gimei/address.ex
@@ -7,6 +7,10 @@ defmodule Gimei.Address do
     IO.puts city()
   end
 
+# TODO: fakerのように、読み込んだ値に対してeachを適用し、そのキーの場合分けで実装する。
+# values.each data, fn {"prefecture", values} -> ... end {"city", values} -> ... end {"town", values} -> .. end
+# https://github.com/igas/faker/blob/master/lib/faker/app.ex
+
   defp city() do
     {_, values} = List.flatten(:yamerl_constr.file(@data_path)) |> Enum.at(0)
     {_, cities} = List.keyfind(values, 'city', 0)
